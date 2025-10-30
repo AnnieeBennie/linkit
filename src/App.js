e4f2a2f60253452413e43575ee28d9f8e66418b8
salmaalix <salmaalixirsi@gmail.com>
2025-10-30 23:18:34 +0100

    Finished first sprint of HomePage with mock data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Header from "./Components/Header";
+import HomePage from "./Pages/HomePage";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Header />
       <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/clubs" element={<div>Clubs Page</div>} />
         <Route path="/calendar" element={<div>Calendar Page</div>} />
         <Route path="/events" element={<div>Events Page</div>} />
         <Route path="/chats" element={<div>Chats Page</div>} />
         <Route path="/settings" element={<div>Settings Page</div>} />
-        <Route path="/home" element={<div>Home Page</div>} />
+        <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
