28d8edbcbf0aaf33e4be68b2a1da29a479e5bcc4
AnnieeBennie <annasemeriuk@gmail.com>
2025-11-20 13:50:56 +0100

    added popup for event details

--- src/Components/EventDetails.js
+++ src/Components/EventDetails.js
@@ -3,35 +3,59 @@ import "../css/EventDetails.css";
 import TicketIcon from "../Icons/Ticket.svg";
 import TimeIcon from "../Icons/Time Circle.svg";
 import LocationIcon from "../Icons/Location.svg";
+import CloseIcon from "../Icons/close.svg";
 
-function eventDetails({ event }) {
+function EventDetails({ event, onClose }) {
   return (
     <div className="details-container">
-      {event.image ? (
-        <img src={event.image} alt={event.title} className="event-image" />
-      ) : (
-        <div className="event-image placeholder" aria-hidden="true" />
-      )}
-      <h2 className="Title">{event.title}</h2>
-      <div>
-        <img src={TicketIcon} alt="Organizer" className="icon"></img>
-        <p className="event-org">{event.organizer}</p>
-      </div>
-      <div>
-        <img src={TimeIcon} alt="Time" className="icon"></img>
-        <p className="event-date">{event.date}</p>
-      </div>
-      <div>
-        <img src={LocationIcon} alt="Location" className="icon"></img>
-        <p className="event-location" title={event.location}>
-          {event.location}
-        </p>
-      </div>
+      <button
+        className="details-close"
+        onClick={onClose}
+        aria-label="Close event details"
+      >
+        <img src={CloseIcon} alt="close" />
+      </button>
 
-      <p className="description">{event.description}</p>
-      <button className="signup-button">Sign Up</button>
-      <button className="add-to-my-calendar">Add to my Calendar</button>
-      <button className="group-chat">Group Chat</button>
+      <div className="details-inner">
+        <div className="details-left">
+          <h2 className="title">{event.title}</h2>
+
+          <div className="info-row">
+            <img src={TicketIcon} alt="Organizer" className="icon" />
+            <p className="event-org">{event.organizer}</p>
+          </div>
+
+          <div className="info-row">
+            <img src={TimeIcon} alt="Time" className="icon" />
+            <p className="event-date">{event.date}</p>
+          </div>
+
+          <div className="info-row">
+            <img src={LocationIcon} alt="Location" className="icon" />
+            <p className="event-location" title={event.location}>
+              {event.location}
+            </p>
+          </div>
+
+          <p className="description">{event.description}</p>
+
+          <div className="actions">
+            <button className="signup-details-button">Sign Up</button>
+            <button className="add-to-my-calendar">Add to my calendar</button>
+            <button className="group-chat">Group Chat</button>
+          </div>
+        </div>
+
+        <div className="details-right">
+          {event.image ? (
+            <img src={event.image} alt={event.title} className="event-image" />
+          ) : (
+            <div className="event-image placeholder" aria-hidden="true" />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
+
+export default EventDetails;
