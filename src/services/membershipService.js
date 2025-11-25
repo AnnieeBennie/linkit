19e85fa76b5babea04b57da8ba95e3c5a8c5ccf3
Christina Polyzogopoulou <christinapol09@gmail.com>
2025-11-25 16:19:13 +0100

    fixed issues from sprint2

--- src/services/membershipService.js
+++ src/services/membershipService.js
@@ -5,12 +5,12 @@ export async function joinClub(clubId) {
   const Membership = Parse.Object.extend("Membership");
   const m = new Membership();
 
-  m.set("user", Parse.User.current());
-  m.set("Club", {
-    __type: "Pointer",
-    className: "Clubs",
-    objectId: clubId,
-  });
+  const user = Parse.User.current();
+  m.set("user", user);
+
+  const club = new Parse.Object("Clubs");
+  club.id = clubId;
+  m.set("Club", club);
 
   return await m.save();
 }
@@ -19,19 +19,19 @@ export async function joinClub(clubId) {
 export async function leaveClub(clubId) {
   const query = new Parse.Query("Membership");
   query.equalTo("user", Parse.User.current());
-  query.equalTo("Club", {
-    __type: "Pointer",
-    className: "Clubs",
-    objectId: clubId,
-  });
 
+  const club = new Parse.Object("Clubs");
+  club.id = clubId;
+  query.equalTo("Club", club);
+
+  //Find the membership entry
   const membership = await query.first();
   if (membership) {
     return membership.destroy();
   }
 }
 
-// Load all clubs joined by current user
+// Load all clubs the user is a member of
 export async function loadJoinedClubs() {
   const query = new Parse.Query("Membership");
   query.equalTo("user", Parse.User.current());
